42f82f916059ba9a6b471163d43c91abbb6ecbcf
Jose Puentes <joseft40@gmail.com>
2023-06-29 04:26:27 -0400

    fix - cambio en el background del modal del login

--- js/tienda_autologin.js
+++ js/tienda_autologin.js
@@ -2,7 +2,8 @@
   Drupal.behaviors.tienda_autologin = {
     attach: function (context, settings) {
       let $url_host = drupalSettings.tienda_autologin.redirect_host;
-      let $uid = drupalSettings.tienda_autologin.user_external;
+      let $uid = drupalSettings.tienda_autologin.user_external.uid;
+      let $has = drupalSettings.tienda_autologin.user_external.has;
 
       if ($(context).find(".E-enlaces-adicionales-menu .enlace-area a").length && ($uid > 0)) {
         //Ocultar el modal
@@ -13,7 +14,6 @@
                 <section class="E-espacio-cabecera G-fondo--blanco">
                   <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close" style="float:right; padding-right: 25px; margin-top: -10px; box-shadow: none;"></button>
                   <div class="G-max--700 G-margen--auto" style="padding: 44px 20px 120px">
-                    <h3 style="padding-left: 44px; padding-right: 44px; font-weight: bold" class="G-txt--mayus G-txt--cen G-color--primario G-txt--xl G-margen--xxm G-margen--lr-0 G-margen--top-0">Editar usuario</h3>
                     <div id="load-edit-user-form-content"></div>
                   </div>
                 </section>
@@ -28,9 +28,10 @@
           $('#modal-load-edit-user-form-auto-login').addClass('modal-edit-user-show show');
 
           let $url_redirect = encodeURIComponent(drupalSettings.tienda_autologin.redirect_edituser);
-          let $url_site = `${$url_host}/user/${$uid}/edit?redirect=${$url_redirect}&autologin=true&op=edituser`;
+          $has = encodeURIComponent($has);
+          let $url_site = `${$url_host}/userautenticacion/${$has}/edituser?redirect=${$url_redirect}`;
           
-          let height = $(window).height() - 100;
+          let height = $(window).height();
           $('#modal-load-edit-user-form-auto-login').find('h3').text("Editar Usuario");
 	      $('#modal-load-edit-user-form-auto-login').find("#load-edit-user-form-content").prepend(`<iframe id="iframe_set_password_form" title="Editar usuario" width="580" height="${height}" src="${$url_site}" frameBorder="0"></iframe>`);
         });
@@ -83,7 +84,7 @@
           let $url_redirect = encodeURIComponent(drupalSettings.tienda_autologin.redirect_register);
           let $url_site = `${$url_host}/user/register?redirect=${$url_redirect}&autologin=true&op=register`;
 
-          let height = $(window).height() - 300;
+          let height = $(window).height();
 	      $('#modal-load-register-form-auto-login').find("#load-register-form-content").prepend(`<iframe id="iframe_register_form" title="Registro de usuario" width="580" height="${height}" src="${$url_site}" frameBorder="0"></iframe>`);
 
         });
